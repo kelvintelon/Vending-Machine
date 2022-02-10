cf35e7b9d62aa1b5d2df61967961b44e971c2115
Phillip Baucom <fsuphil10@gmail.com>
2022-02-10 14:39:29 -0500

    Purchase Options

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -10,13 +10,16 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String EXIT_MAIN_MENU = "Exit";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
-	private double balance;
-	private static final String FEED_MONEY = "Add money";
+	private double balance = 0.00;
+	private static final String FEED_MONEY = "Feed money";
 	private static final String FEED_MONEY_ONE = "Add One Dollar";
 	private static final String FEED_MONEY_TWO = "Add Two Dollars";
 	private static final String FEED_MONEY_FIVE = "Add Five Dollars";
 	private static final String FEED_MONEY_TEN = "Add Ten Dollars";
 	private static final String[] FEED_MENU_OPTIONS = { FEED_MONEY_ONE, FEED_MONEY_TWO, FEED_MONEY_FIVE, FEED_MONEY_TEN};
+	private static final String SELECT_PRODUCT = "Select Product";
+	private static final String FINISH_TRANSACTION = "Final Transaction";
+	private static final String[] PURCHASE_MENU_OPTIONS = {FEED_MONEY, SELECT_PRODUCT, FINISH_TRANSACTION};
 
 
 
@@ -36,16 +39,39 @@ public class VendingMachineCLI {
 				displayVendingMachineInventory();
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
-				if  {
-
-				}
-					menu.getChoiceFromOptions(FEED_MENU_OPTIONS);
+				purchaseOptions();
 
 			} else if (choice.equals(EXIT_MAIN_MENU)) {
 				// exit
 			}
 		}
 	}
+
+	public void purchaseOptions(){
+		while (true) {
+			String choice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+
+			if (choice.equals(FEED_MONEY)) {
+				// handle money method
+				String moneyChoice = (String) menu.getChoiceFromOptions(FEED_MENU_OPTIONS);
+				if (moneyChoice.equals(FEED_MONEY_ONE)) {
+					addBalance(1.00);
+				} if (moneyChoice.equals(FEED_MONEY_TWO)) {
+					addBalance(2.00);
+				} if (moneyChoice.equals(FEED_MONEY_FIVE)) {
+					addBalance(5.00);
+				} if (moneyChoice.equals(FEED_MONEY_TEN)) {
+					addBalance(10.00);
+				}
+			} else if (choice.equals(SELECT_PRODUCT)) {
+				//select product method
+			} else if (choice.equals(FINISH_TRANSACTION)) {
+				// finish transaction method
+			}
+			System.out.println("Current Money Provided: $" + balance);
+		}
+	}
+
 
 	public static void main(String[] args) {
 		Menu menu = new Menu(System.in, System.out);
@@ -54,7 +80,9 @@ public class VendingMachineCLI {
 	}
 
 
-
+	public void productSelection() {
+		
+	}
 
 
 
@@ -71,8 +99,16 @@ public class VendingMachineCLI {
 	}
 
 
-	public void addBalance(int money) {
-		this.balance = this.balance + money;
-		System.out.println("Balance is " + balance);
+	public void addBalance(double money) {
+		if (money == 1.00) {
+			balance += 1.00;
+		} if (money == 2.00) {
+			balance += 2.00;
+		} if (money == 5.00) {
+			balance += 5.00;
+		}if (money == 10.00) {
+			balance += 10.00;
+		}
+
 	}
 }
