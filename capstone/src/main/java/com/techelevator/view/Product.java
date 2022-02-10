b5b5e67de4049869250211d0496c40b37fe60665
Phillip Baucom <fsuphil10@gmail.com>
2022-02-10 10:20:01 -0500

    1st push

---
+++ capstone/src/main/java/com/techelevator/view/Product.java
@@ -0,0 +1,11 @@
+package com.techelevator.view;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String name;
+    private int inventory;
+    private BigDecimal price;
+    private String slotLocation;
+}
