1086f69d8ce304ff2dbd623c1fe2251a0db62e89
Kelvin <kelvin.telon@techelevator.com>
2022-10-10 16:12:04 -0400

    added vending machine buttons and input, still needs display and functionality

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,29 +1,166 @@
 <template>
   <body class="home">
-    <h1 class="title" style = "position:relative; top:25px;">Vending Machine</h1>
-    <img alt="Vending Machine" src="../assets/vending.png" style = "position:relative; top:25px;" class="blur" id="image">
+    <h1 class="title" style="position: relative; top: 25px">Vending Machine</h1>
+    <img
+      alt="Vending Machine"
+      src="../assets/vending.png"
+      style="position: relative; top: 25px"
+      class="blur"
+      id="image"
+    />
+    <div class="vmPicker">
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">#</span>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Slot Location"
+          aria-label="Slot Location"
+          aria-describedby="button-addon2"
+          v-model="slotLocation"
+        />
+        <button
+          class="btn btn-primary"
+          type="button"
+          id="button-addon2"
+        >
+          Buy!
+        </button>
+      </div>
+
+      <div
+        class="btn-toolbar justify-content-between"
+        role="toolbar"
+        aria-label="Toolbar with button groups"
+      >
+        <div class="btn-group" role="group" aria-label="First group">
+          <button type="radio" class="btn btn-outline-warning" v-on:click="changeSlot($event)">A1</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">A2</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">A3</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">A4</button>
+        </div>
+      </div>
+      <div
+        class="btn-toolbar justify-content-between"
+        role="toolbar"
+        aria-label="Toolbar with button groups"
+      >
+        <div class="btn-group" role="group" aria-label="First group">
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">B1</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">B2</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">B3</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">B4</button>
+        </div>
+      </div>
+      <div
+        class="btn-toolbar justify-content-between"
+        role="toolbar"
+        aria-label="Toolbar with button groups"
+      >
+        <div class="btn-group" role="group" aria-label="First group">
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">C1</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">C2</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">C3</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">C4</button>
+        </div>
+      </div>
+      <div
+        class="btn-toolbar justify-content-between"
+        role="toolbar"
+        aria-label="Toolbar with button groups"
+      >
+        <div class="btn-group" role="group" aria-label="First group">
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">D1</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">D2</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">D3</button>
+          <button type="button" class="btn btn-outline-warning" v-on:click="changeSlot($event)">D4</button>
+        </div>
+      </div>
+      <div style="padding-bottom: 10px">Total Money: $</div>
+      <div class="btn-group">
+        <button
+          type="button"
+          class="btn btn-danger dropdown-toggle"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Feed Money
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">$1</a></li>
+          <li><a class="dropdown-item" href="#">$5</a></li>
+          <li><a class="dropdown-item" href="#">$10</a></li>
+        </ul>
+      </div>
+    </div>
   </body>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'Home',
-  components: {
-    
+  name: "Home",
+  data() {
+    return {
+      slotLocation: "",
+    };
+  },
+  components: {},
+  methods: {
+    changeSlot(event) {
+      this.slotLocation = event.target.innerText;
+
+    }
   }
-}
+};
 </script>
 
 <style>
+.vmPicker {
+  color: rgb(66, 135, 214);
+  float: left;
+  position: absolute;
+  left: 38%;
+  top: 300px;
+  z-index: 1000;
+  color: rgb(66, 135, 214);
+  border-color: rgb(66, 135, 214);
+  border-style: solid;
+  border-radius: 60px;
+  padding: 5px;
+  height: 42%;
+  width: 25%;
+  font-weight: bold;
+  background: #00050b;
+  animation: fadeIn 5s;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.btn-toolbar {
+  padding-bottom: 11px;
+}
+.mb-3 {
+  padding-top: 10px;
+  width: 70%;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
 .home {
   background: #00050b;
-  font-family: 'Brush Script MT', cursive;
-  
+  font-family: "Brush Script MT", cursive;
+
   display: flex;
-  flex-direction: column; 
+  flex-direction: column;
 }
 
 .title {
@@ -35,12 +172,10 @@ export default {
   width: 500px;
   display: flex;
   justify-content: center;
-  margin:0 auto;
-  
+  margin: 0 auto;
 }
 
 .image {
-  
   /* display: flex;
   flex-basis: 100%; */
 }
@@ -79,5 +214,4 @@ export default {
     filter: blur(5px);
   }
 }
-
 </style>
